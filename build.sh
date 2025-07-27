b5f01963d308ed4444713a2a4dff93493a7a8d19
collagewebsite <jay473039@gmail.com>
2025-07-27 18:02:53 +0530

    add files

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# wkhtmltopdf install
+echo "Installing wkhtmltopdf..."
+apt-get update
+apt-get install -y wkhtmltopdf
--- vp/views.py
+++ vp/views.py
@@ -126,8 +126,10 @@ def upload_syllabus(request):
 
 
 # wkhtmltopdf चा path set कर
-path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'  # Make sure 'bin' path is added
-config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+# path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'  # Make sure 'bin' path is added
+# config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
+pdfkit.from_string("Hello World", "out.pdf", configuration=config)
 
 @csrf_exempt
 def download_pdf(request):
